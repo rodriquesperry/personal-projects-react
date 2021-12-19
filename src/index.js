151ee457ab3a0fb7ef89ed1ad587fd1e825c4e38
Rodriques <rodriques.perry@gmail.com>
2021-12-19 13:09:49 -0600

    complete: React.useState

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  const [developer, setDeveloper] = React.useState({
+    language: "Python",
+    yearsOfExperience: 0,
+  });
+  // const [language, setLanguage] = React.useState("python");
+  // const [yearsOfExperience, setYearsOfExperience] = React.useState(0);
+  function handleBUttonClick(e) {
+    setDeveloper({
+      language: "JavaScript",
+      yearsOfExperience: 0,
+    });
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  function years(e) {
+    setDeveloper({
+      ...developer,
+      yearsOfExperience: e.target.value,
+    });
+  }
+
+  return (
+    <div>
+      <button onClick={handleBUttonClick}>Button Language</button>
+      <div>
+        <input type="number" onChange={years} />
+      </div>
+
+      <p>I am learning {developer.language}</p>
+      <p>
+        I have {developer.yearsOfExperience} in {developer.language}
+      </p>
+    </div>
+  );
+}
+
+const rootNode = document.getElementById("root");
+ReactDOM.render(<App />, rootNode);
